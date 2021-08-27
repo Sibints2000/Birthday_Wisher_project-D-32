d27012f25226de065909dc705d8df589bd339112
Sibints2000 <sibints2000@gmail.com>
2021-08-27 11:16:56 +0530

    Seventeenth Commit

--- main.py
+++ main.py
@@ -1,13 +1,39 @@
-import smtplib
 
-my_email = "thurrappattujoseph@gmail.com"
-password = "thurrappattu@007"
 
-with smtplib.SMTP("smtp.gmail.com") as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    connection.sendmail(
-        from_addr=my_email,
-        to_addrs="sibin321@yahoo.com",
-        msg="Subject:Hello\n\nThis is the bod of my email"
-    )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import smtplib
+#
+# my_email = "thurrappattujoseph@gmail.com"
+# password = "thurrappattu@007"
+#
+# with smtplib.SMTP("smtp.gmail.com") as connection:
+#     connection.starttls()
+#     connection.login(user=my_email, password=password)
+#     connection.sendmail(
+#         from_addr=my_email,
+#         to_addrs="sibin321@yahoo.com",
+#         msg="Subject:Hello\n\nThis is the bod of my email"
+#     )
