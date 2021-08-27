63c0b905911dcc9a342e45d18a86bcdf1584c70a
Sibints2000 <sibints2000@gmail.com>
2021-08-27 11:30:19 +0530

    Thirty Second Commit

--- main.py
+++ main.py
@@ -25,16 +25,16 @@ if weekday == 4:
 
 
 
-# import smtplib
-#
-# my_email = "thurrappattujoseph@gmail.com"
-# password = "thurrappattu@007"
-#
-# with smtplib.SMTP("smtp.gmail.com") as connection:
-#     connection.starttls()
-#     connection.login(user=my_email, password=password)
-#     connection.sendmail(
-#         from_addr=my_email,
-#         to_addrs="sibin321@yahoo.com",
-#         msg="Subject:Hello\n\nThis is the bod of my email"
-#     )
+import smtplib
+
+my_email = "thurrappattujoseph@gmail.com"
+password = "thurrappattu@007"
+
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=password)
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs="sibin321@yahoo.com",
+        msg="Subject:Hello\n\nThis is the bod of my email"
+    )
