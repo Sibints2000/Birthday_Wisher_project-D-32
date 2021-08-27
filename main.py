2f950365793135a21fd865b9c3ed2687e1809cd0
Sibints2000 <sibints2000@gmail.com>
2021-08-27 10:37:27 +0530

    Seventh Commit

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import smtplib
 
 my_email = "thurrappattujoseph@gmail.com"
+password = "thurrappattu@007"
 
 connection = smtplib.SMTP("smtp.gmail.com")
 connection.starttls()
-
+connection.login(user=my_email, password=password)
