8dd47c3fcc1d4b99200dd7340f97c5a885d7671e
Sibints2000 <sibints2000@gmail.com>
2021-08-27 10:38:37 +0530

    Eighth Commit

--- main.py
+++ main.py
@@ -6,3 +6,4 @@ password = "thurrappattu@007"
 connection = smtplib.SMTP("smtp.gmail.com")
 connection.starttls()
 connection.login(user=my_email, password=password)
+connection.sendmail(from_addr=my_email, to_addrs="sibin321@yahoo.com")
