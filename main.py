b50de071eca59e05b1459921036c0606f8273bf9
Sibints2000 <sibints2000@gmail.com>
2021-08-27 10:39:35 +0530

    Ninth Commit

--- main.py
+++ main.py
@@ -6,4 +6,5 @@ password = "thurrappattu@007"
 connection = smtplib.SMTP("smtp.gmail.com")
 connection.starttls()
 connection.login(user=my_email, password=password)
-connection.sendmail(from_addr=my_email, to_addrs="sibin321@yahoo.com")
+connection.sendmail(from_addr=my_email, to_addrs="sibin321@yahoo.com", msg="Hello")
+connection.close()
