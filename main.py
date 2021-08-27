febf307c0ebcc68efac6821a5cea293c6e4b8277
Sibints2000 <sibints2000@gmail.com>
2021-08-27 11:23:10 +0530

    Twenty Fourth Commit

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ import random
 
 now = dt.datetime.now()
 weekday = now.weekday()
-if weekday == 5:
+if weekday == 4:
     with open("quotes.txt") as quote_file:
         all_quotes = quote_file.readlines()
         quote = random.choice(all_quotes)
+
+    print(quote)
 
 
 
