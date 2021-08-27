f9c1601730c6c44f59e61fa3393e79c2f5333cc7
Sibints2000 <sibints2000@gmail.com>
2021-08-27 11:28:12 +0530

    Thirtieth Commit

--- main.py
+++ main.py
@@ -16,7 +16,11 @@ if weekday == 4:
     with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
         connection.login(MY_EMAIL, MY_PASSWORD)
-
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=MY_EMAIL,
+            msg=f"Subject:Monday Motivation\n\n{quote}"
+        )
 
 
 
