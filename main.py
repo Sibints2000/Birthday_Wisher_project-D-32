0af3aeb2c0f6e3029e64d26d07eb72d55216d40e
Sibints2000 <sibints2000@gmail.com>
2021-08-27 11:24:10 +0530

    Twenty Fifth Commit

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import smtplib
 import datetime as dt
 import random
 
+MY_EMAIL = "thurrappattujoseph@gmail.com"
+
+
 now = dt.datetime.now()
 weekday = now.weekday()
 if weekday == 4:
@@ -10,6 +13,7 @@ if weekday == 4:
         quote = random.choice(all_quotes)
 
     print(quote)
+    connection = smtplib.SMTP("")
 
 
 
